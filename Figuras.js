384c48ee15857aac0c8827c424b72da0f24c43ef
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2021-07-30 15:22:14 -0400

    calculo de figuras geometricas

---
+++ Figuras.js
@@ -0,0 +1,69 @@
+// Código del cuadrado
+console.group("cuadrados");
+
+// const ladoCuadrado = 5;
+// console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm.");
+
+function perimetroCuadrado(lado) {
+    return lado * 4;
+}
+// console.log("El perimetro del cuadrado mide: " + perimetroCuadrado + " cm.");
+
+function areaCuadrado(lado) {
+    return lado * lado;
+}
+// console.log("El área del cuadrado mide: " + areaCuadrado + " cm²");
+
+console.groupEnd();
+// Código del triangulo
+console.group("Tríangulos");
+
+// const ladoTriangulo1 = 6;
+// const ladoTriangulo2 = 6;
+// const baseTriangulo = 4;
+// console.log(
+//     "Los lados del cuadrado miden: "
+//     + ladoTriangulo1 + " cm, "
+//     + ladoTriangulo2 + " cm y "
+//     + baseTriangulo + " cm."
+// );
+
+// const alturaTriangulo = 5.5;
+// console.log("La altura del triángulo mide: " + alturaTriangulo + " cm.");
+
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+}
+// console.log("El perimetro del triángulo mide: " + perimetroTriangulo + " cm.");
+
+function areaTraingulo(base, altura) {
+    return (base + altura) / 2;
+}
+// console.log("El área del triángulo mide: " + areaTraingulo + " cm²");
+
+console.groupEnd();
+
+// Código del círculo
+console.group("Círculos");
+
+// const radioCirculo = 4;
+// console.log("El radio del circulo mide: " + radioCirculo + " cm.");
+
+function diametroCirculo(radio) {
+    return radio * 2;
+}
+// console.log("El diámetro del circulo mide: " + diametro + " cm.");
+
+const PI = Math.PI;
+// console.log("PI es: " + PI);
+
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+}
+function areaCirculo(radio) {
+    return (radio * radio) * PI;
+}
+//console.log("El área del circulo mide: " + areaCirculo + " cm.");
+
+console.groupEnd();
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Javascript-practico
 Curso practico de JavaScript [Platzi]
+
+...
+
+## Taller #1: Figuras geométricas
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar las formulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JavaScript con HTML
